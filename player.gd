494b371518d7c1ade139760cd46241cbf1407827
Dexter Liu <dexterliu@LP2-US-51719311.local>
2021-10-03 11:36:37 -0700

    Initial import of unstable

---
+++ player.gd
@@ -0,0 +1,43 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var control_node
+var camera_posn
+var int_velocity = Vector2(0,0)
+var speed = 300
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	control_node = get_node("/root/controller")
+	camera_posn =  get_node("/root/controller/player/Camera2D").get_camera_position()	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	int_velocity = Vector2(0,0)
+	
+	if Input.is_action_pressed("ui_right"):
+		int_velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		int_velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		int_velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		int_velocity.y -= 1
+		
+	if int_velocity.length() > 0:		
+		int_velocity = int_velocity.normalized() * speed * 2.5
+
+	self.set_linear_velocity(int_velocity)
+					
+	pass
+
+
+func _draw():
+	draw_circle(Vector2(0,0), 10, Color(1,1,1,0.8))
