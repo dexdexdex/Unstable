02484e6df986302143a22d54fe45d08e3493cc90
Dexter Liu <dexterliu@LP2-US-51719311.local>
2021-10-03 14:38:27 -0700

    Implement preliminary collisions

--- controller.gd
+++ controller.gd
@@ -13,9 +13,13 @@ var player_node
 
 var new_ghost_object
 
+var ghost_number = 0
+
 export(PackedScene) var ghost_object
 
 var player_steps = 0
+
+var detected = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -35,27 +39,25 @@ func _process(delta):
 	}
 	
 	player_state.append(frame_data)
-	print('player state has ', player_state.size(), ' entries')
 
 	if Input.is_action_pressed("ui_q"):
-		print('stop recording')
-		# print(player_state)
-		print('player steps is at ', player_steps)
 		
 		var ghost_player_steps = player_steps
 		var ghost_player_state = player_state
 		new_ghost_object = ghost_object.instance()
 		new_ghost_object.load_ghost_data(ghost_player_state)
 		new_ghost_object.set_ghost_max_steps(ghost_player_steps)
-		
+		new_ghost_object.position.x = ghost_player_state[0].x
+		new_ghost_object.position.y = ghost_player_state[0].y
+		new_ghost_object.set_ghost_number(ghost_number)
+			
 		add_child(new_ghost_object)
-		
+		ghost_number = ghost_number + 1
 		
 	pass
 	
 	
 	if Input.is_action_pressed("ui_w"):
-		print('reset time')
 		player_steps = 0
 		player_state = []		
 		
--- ghost.gd
+++ ghost.gd
@@ -1,27 +1,47 @@
 # ghost object, play back and draw where you are
 
-extends Node2D
+extends KinematicBody2D
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 export(Color, RGBA) var ghost_color = Color(1,1,1,1)
+export(Color, RGBA) var ghost_circle_color
+
 var ghost_state = []
 var counter = 0
 var ghost_max_steps = 0
+
+var player_node
+var ghost_number
 
 func load_ghost_data(data):
 	ghost_state = data
 	pass
 
 func set_ghost_max_steps(count):
-	print('setting max steps for ghost at ', count)
 	ghost_max_steps = count
+
+func set_ghost_number(number):
+	ghost_number = number
+
+func draw_circle_arc(center, radius, angle_from, angle_to, color):
+	var nb_points = 32
+	var points_arc = PoolVector2Array()
+	
+	for i in range(nb_points + 1):
+		var angle_point = deg2rad(angle_from + i * (angle_to-angle_from) / nb_points - 90)
+		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+
+	for index_point in range(nb_points):
+		draw_line(points_arc[index_point], points_arc[index_point + 1], color)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	player_node = get_node("/root/controller/player")	
 	# load_ghost_data(data)
 	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,11 +52,17 @@ func _process(delta):
 		
 	position.x = ghost_state[counter].x
 	position.y = ghost_state[counter].y
-	
-	print('hello i am a ghost at ', position)
-	
-	pass
+
+
+	var distance_to_player = (self.get_global_position()).distance_to(player_node.get_position())
+	if(distance_to_player <= 100):
+		print('super super DETECTED by ', ghost_number)
 
 
 func _draw():
 	draw_circle(Vector2(0,0), 10, ghost_color)
+	
+	var angle_from = 0
+	var angle_to = 360
+	var radius = 100
+	draw_circle_arc(Vector2(0,0), radius, angle_from, angle_to, ghost_circle_color)	
